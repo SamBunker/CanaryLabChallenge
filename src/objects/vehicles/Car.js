7b992c6bc1a0ae159cc59e84547d0c0a1257b7af
Not Sam <samuelbunker@live.com>
2025-11-12 18:04:20 -0500

    Corrected hourly rate to an int instead of string.

--- src/objects/vehicles/Car.js
+++ src/objects/vehicles/Car.js
@@ -2,7 +2,7 @@ import Vehicle from './Vehicle.js';
 
 class Car extends Vehicle {
     constructor() {
-        super('car', 'medium', '2');
+        super('car', 'medium', 2);
     }
 }
 export default Car;
