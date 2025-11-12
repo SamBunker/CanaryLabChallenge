0f4e0aa6b20b14958827afb000744823769a4a3e
Not Sam <samuelbunker@live.com>
2025-11-11 21:40:29 -0500

    Created base classes for factory, CreateVehicle and CreateSpot

--- src/factory/CreateSpot.js
+++ src/factory/CreateSpot.js
@@ -1,3 +1,22 @@
+const SmallSpot = require('../objects/spots/SmallSpot');
+const MediumSpot = require('../objects/spots/MediumSpot');
+const LargeSpot = require('../objects/spots/LargeSpot');
+
 class CreateSpot {
-    
+    static create(size, floor, spotNumber) { 
+        const spotSize = size.toLowerCase().trim();
+
+        switch(spotSize) {
+            case 'small':
+                return new SmallSpot(floor, spotNumber);
+            case 'medium':
+                return new MediumSpot(floor, spotNumber);
+            case 'large':
+                return new LargeSpot(floor, spotNumber);
+            default:
+                throw new Error('Invlaid spot size: ${size}. Valid sizes: small, medium, large');
+        }
+    }
 }
+
+module.exports = CreateSpot;
--- src/factory/CreateVehicle.js
+++ src/factory/CreateVehicle.js
@@ -1,3 +1,22 @@
-class CreateVehicle {
+const Bike = require('../objects/vehicles/Bike');
+const Car = require('../objects/vehicles/Car');
+const Truck = require('../objects/vehicles/Truck');
 
+class CreateVehicle {
+    static create(type) {
+        const vehicleType = type.toLowerCase().trim();
+
+        switch(vehicleType) {
+            case 'bike':
+                return new Bike();
+            case 'car':
+                return new Car();
+            case 'truck':
+                return new Truck();
+            default:
+                throw new Error('Invlaid vehicle type: ${type}. Valid types: bike, car, truck');
+        }
+    }
 }
+
+module.exports = CreateVehicle;
