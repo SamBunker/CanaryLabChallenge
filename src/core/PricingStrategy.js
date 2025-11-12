48d48a0de0914afabfe4bf38f9baa601ba2dc1cd
Not Sam <samuelbunker@live.com>
2025-11-11 21:58:58 -0500

    Added ticket class for handling data, Added PricingStrategy.js for calculating the parking fees according to the vehicle's size

--- src/core/PricingStrategy.js
+++ src/core/PricingStrategy.js
@@ -1,3 +1,11 @@
 class PricingStrategy {
+    static calculateFee(vehicle, durationMinutes) {
+        const hours = Math.ceil(durationMinutes / 60);
+        const rate = vehicle.getHourlyRate();
+        return hours * rate;
+    }
     
+    static formatFee(fee) { return '$${fee.toFixed(2)}'; }
 }
+
+module.exports = PricingStrategy;
--- src/core/Ticket.js
+++ src/core/Ticket.js
@@ -1,3 +1,13 @@
 class Ticket {
-    
+    constructor(id, vehicle, spot) {
+        this.id = id;
+        this.vehicle = vehicle;
+        this.spot = spot;
+        this.entryTime = Date.now();
+    }
+
+    getDuration() { return Date.now() - this.entryTime; }
+    getEntryTimeFormatted() { return new Date(this.entrtyTime).toLocaleString(); }    
 }
+
+module.exports = Ticket;
